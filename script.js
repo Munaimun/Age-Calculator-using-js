8aad3cf2d7ac7b9df851048e1cb12d181fcaabb9
Munaimun <munaimunz@gmail.com>
2023-09-17 20:30:21 +0600

    fixed calculation issue in script.js

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
-    function age() {
+function age() {
     // input birthdate
     var d1 = parseInt(document.getElementById("date").value);
     var m1 = parseInt(document.getElementById("month").value);
     var y1 = parseInt(document.getElementById("year").value);
 
-    //getiing current date
+    // getting current date
     var date = new Date();
-    var d2 = date.getDate(); 
+    var d2 = date.getDate();
     var m2 = 1 + date.getMonth(); //current month
     var y2 = date.getFullYear();
-    // var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
     var f1, f2, f3;
-    if(d1 > 0 && d1 <= 31 ){
+    if (d1 > 0 && d1 <= 31) {
         f1 = true;
-    }else {
-        alert("invalid date");
+    } else {
+        alert("Invalid date");
     }
-    if(m1 <= 12 && m1>0){
+    if (m1 <= 12 && m1 > 0) {
         f2 = true;
-    }else{
-        alert("invalid month");
+    } else {
+        alert("Invalid month");
     }
-    if(y1 <= y2 && y1>0){
+    if (y1 <= y2 && y1 > 0) {
         f3 = true;
-    }else{
-        alert("invalid year");
+    } else {
+        alert("Invalid year");
     }
 
-    if(f1 === true && f2 ===true && f3 === true){
-    var d = Math.abs(d2 - d1);
-    var m = Math.abs(m2 - m1);
-    var y = Math.abs(y2 - y1);
-    document.getElementById("age").innerHTML =
-        "Your Age is " + y + " Years " + m + " Months " + d + " Days";
-    }else{
-         d1 = document.getElementById("date").value="";
-         m1 = document.getElementById("month").value="";
-         y1 = document.getElementById("year").value="";
-        
+    if (f1 === true && f2 === true && f3 === true) {
+        var ageYears = y2 - y1;
+        var ageMonths = m2 - m1;
+        var ageDays = d2 - d1;
+
+        if (ageDays < 0) {
+            ageMonths--;
+            var daysInPreviousMonth = new Date(y2, m2 - 1, 0).getDate();
+            ageDays = daysInPreviousMonth + ageDays;
+        }
+
+        if (ageMonths < 0) {
+            ageYears--;
+            ageMonths = 12 + ageMonths;
+        }
+
+        if (ageYears > 0) {
+            document.getElementById("age").textContent =
+                "Your Age is " + ageYears + " Years " + ageMonths + " Months " + ageDays + " Days";
+        } else if (ageMonths > 0) {
+            document.getElementById("age").textContent =
+                "Your Age is " + ageMonths + " Months " + ageDays + " Days";
+        } else {
+            document.getElementById("age").textContent =
+                "Your Age is " + ageDays + " Days";
+        }
+    } else {
+        document.getElementById("date").value = "";
+        document.getElementById("month").value = "";
+        document.getElementById("year").value = "";
     }
-   
 }
